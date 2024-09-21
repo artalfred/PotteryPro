d6c9313f73afecd2a39a150bed1cf37a3a1e7004
art alfred <124093565+artalfred@users.noreply.github.com>
2024-09-22 00:47:13 +0800

    Update authRoutes.js

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -9,28 +9,20 @@ const {
   logout,
 } = require("../controllers/authController");
 
-// MIDDLEWARE
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://pottery-pro-71wh.vercel.app'); // Only allow your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+router.use(
+  cors({
+    credentials: true,
+    origin: "https://pottery-pro-71wh.vercel.app/",
+    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+    allowedHeaders:
+      "Content-Type, Authorization, X-Requested-With, X-CSRF-Token",
+  })
+);
 
-
-// Apply `allowCors` to all routes
-router.get("/", allowCors(test));
-router.post("/register", allowCors(registerUser));
-router.post("/login", allowCors(loginUser));
-router.get("/profile", allowCors(getProfile));
-router.post("/logout", allowCors(logout));
+router.get("/", test);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", getProfile);
+router.post("/logout", logout);
 
 module.exports = router;
