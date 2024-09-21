2480e706a60a9001014f42f2498651a98ecd733f
art alfred <124093565+artalfred@users.noreply.github.com>
2024-09-22 03:27:27 +0800

    Update authRoutes.js

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -14,7 +14,7 @@ router.use(
   cors({
     origin: "https://artfullprotterypro.vercel.app",
     credentials: true,
-    methods: "GET, POST",
+    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",  // Ensure correct headers are allowed
   })
 );
